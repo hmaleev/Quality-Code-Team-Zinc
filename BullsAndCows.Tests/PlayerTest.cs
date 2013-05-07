b386b7169de7aa65b00403a55554d54a887664ae
boyomarinov <boyan.marinov@yahoo.com>
2013-05-07 14:40:22 +0300

    Fixed links to Test Project

---
+++ BullsAndCows.Tests/PlayerTest.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BullsAndCows.Tests
+{
+    public class PlayerTest
+    {
+        [TestMethod]
+        public void TestNickname()
+        {
+            Player player = new Player("pesho", 10);
+            Assert.AreEqual(player.Nickname, "pesho");
+        }
+        [TestMethod]
+
+        [ExpectedException(typeof(ArgumentException))]
+        public void TestEmptyNickName()
+        {
+            Player player = new Player("", 0);
+            Assert.AreEqual(player.Nickname, String.Empty);
+        }
+        [TestMethod]
+        public void TestScore()
+        {
+            Player player = new Player("pesho", 10);
+            Assert.AreEqual(player.Score, 10);
+        }
+
+    }
+}
