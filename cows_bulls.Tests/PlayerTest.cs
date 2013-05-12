5b7916c1aef34e81de25392665e72b42d9cb81fd
boyomarinov <boyan.marinov@yahoo.com>
2013-05-13 01:20:04 +0300

    Added Player Tests for full coverage

--- cows_bulls.Tests/PlayerTest.cs
+++ cows_bulls.Tests/PlayerTest.cs
@@ -19,7 +19,13 @@ namespace cows_bulls.Tests
         public void TestEmptyNickName()
         {
             Player player = new Player("", 0);
-            Assert.AreEqual(player.Nickname, String.Empty);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void TestNullNickname()
+        {
+            Player player = new Player(null, 0);
         }
 
         [TestMethod]
@@ -34,6 +40,42 @@ namespace cows_bulls.Tests
         public void TestNullScore()
         {
             Player player = new Player("pesho", -10000);
+        }
+
+        [TestMethod]
+        public void TestCompareToSmaller()
+        {
+            Player player1 = new Player("player1", 999);
+            Player player2 = new Player("player2", 1000);
+            int result = player1.CompareTo(player2);
+            Assert.AreEqual(-1, result);
+        }
+
+        [TestMethod]
+        public void TestCompareToBigger()
+        {
+            Player player1 = new Player("player1", 1000);
+            Player player2 = new Player("player2", 999);
+            int result = player1.CompareTo(player2);
+            Assert.AreEqual(1, result);
+        }
+
+        [TestMethod]
+        public void TestCompareToWithSameScore()
+        {
+            Player player1 = new Player("player1", 1000);
+            Player player2 = new Player("player2", 1000);
+            int result = player1.CompareTo(player2);
+            Assert.AreEqual(-1, result);
+        }
+
+        [TestMethod]
+        public void TestToString()
+        {
+            Player player = new Player("player", 10);
+            string expected = " 10 | player";
+            string result = player.ToString();
+            Assert.AreEqual(expected, result);
         }
     }
 }
