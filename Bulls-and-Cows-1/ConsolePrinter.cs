fc1f186eba11ee7fbc0e5c88e8d61504ac5f02ec
Marin Vassilev <m.a.vassilev@mail.bg>
2013-05-15 01:14:18 +0300

    ConsolePrinter Class Summarized
    Now everything is summarized :)

--- Bulls-and-Cows-1/ConsolePrinter.cs
+++ Bulls-and-Cows-1/ConsolePrinter.cs
@@ -4,8 +4,14 @@
     using System.Collections.Generic;
     using System.Text;
 
+    /// <summary>
+    /// Class printing on the console
+    /// </summary>
     public class ConsolePrinter
     {
+        /// <summary>
+        /// Static void method writing on the console Intro Message
+        /// </summary>
         public static void PrintWelcomeMessage()
         {
             StringBuilder welcomeMessage = new StringBuilder();
@@ -22,29 +28,47 @@
             Console.WriteLine(welcomeMessage.ToString());
         }
 
+        /// <summary>
+        /// Static void method printing on the console what to do
+        /// </summary>
         public static void PrintEnterGuessMessage()
         {
             Console.Write("Enter your guess or command: ");
         }
 
+        /// <summary>
+        /// Prints on the console if user input is incorrect
+        /// </summary>
         public static void PrintInvalidNumberMessage()
         {
             Console.WriteLine("You have entered an invalid number!");
             Console.WriteLine();
         }
 
+        /// <summary>
+        /// Prints on the console if user input is incorrect
+        /// </summary>
         public static void PrintInvalidCommandMessage()
         {
             Console.WriteLine("You have entered an invalid command!");
             Console.WriteLine();
         }
 
+        /// <summary>
+        /// Printing your current progress
+        /// </summary>
+        /// <param name="bullsCount">Number of bulls</param>
+        /// <param name="cowsCount">Number of cows</param>
         public static void PrintCurrentHits(int bullsCount, int cowsCount)
         {
             Console.WriteLine("Wrong number! Bulls: {0}, Cows: {1}!", bullsCount, cowsCount);
             Console.WriteLine();
         }
 
+        /// <summary>
+        /// Printing on the console hint message
+        /// </summary>
+        /// <param name="hint">Char array of hint</param>
         public static void PrintHint(char[] hint)
         {
             StringBuilder hintMessage = new StringBuilder();
@@ -60,6 +84,11 @@
             Console.WriteLine();
         }
 
+        /// <summary>
+        /// Printing message after finishing the game
+        /// </summary>
+        /// <param name="helpCounter">Count of cheats used</param>
+        /// <param name="guessCounter">Count of attempts</param>
         public static void PrintCongratulationMessage(int helpCounter, int guessCounter)
         {
             StringBuilder congratulationMessage = new StringBuilder();
@@ -78,6 +107,10 @@
             Console.WriteLine();
         }
 
+        /// <summary>
+        /// Static void method printing scoreboard
+        /// </summary>
+        /// <param name="scoreboard">List of players</param>
         public static void PrintScoreboard(List<Player> scoreboard)
         {
             Console.WriteLine();
@@ -110,22 +143,35 @@
             Console.WriteLine();
         }
 
+        /// <summary>
+        /// Static void method printing on the console message to enter user nickname
+        /// </summary>
         public static void PrintEnterNicknameMessage()
         {
             Console.WriteLine("You can add your nickname to top scores!");
             Console.Write("Enter your nickname: ");
         }
 
+        /// <summary>
+        /// Printing information message on the console
+        /// </summary>
         public static void PrintNotAllowedMessage()
         {
             Console.WriteLine("You are not allowed to enter the top scoreboard.");
         }
 
+        /// <summary>
+        /// Printing on the console bye message
+        /// </summary>
         public static void PrintByeMessage()
         {
             Console.WriteLine("Good bye!");
         }
 
+        /// <summary>
+        /// Printing on the console 
+        /// </summary>
+        /// <param name="dashCount">Count of dashes to be printed on the console</param>
         private static void PrintLine(int dashCount)
         {
             for (int i = 0; i < dashCount; i++)
